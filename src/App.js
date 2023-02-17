0dd96ef51cfe4ee34133168af10602d38f2f3573
lDrex1 <damzealwini7@gmail.com>
2023-02-17 13:55:45 +0100

    import: all components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import "./App.css";
+import Footer from "./component/Footer";
+import Header from "./component/Header";
+import Main from "./component/Main";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
